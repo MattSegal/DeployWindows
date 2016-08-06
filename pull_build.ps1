e4067525b3823e9bc0376f33a8dde46e912fab48
MattSegal <mattdsegal@gmail.com>
2016-08-06 13:04:58 +1000

    fix pull build missing directory

--- pull_build.ps1
+++ pull_build.ps1
@@ -1,2 +1,11 @@
-cd "C:\RedditFollower"
+$currentDir = pwd
+
+# Make sure target dir exists
+$targetDir = "C:\RedditFollower"
+if (-ne Test-Path $targetDir) {
+    New-Item $targetDir -type directory
+}
+
+cd $targetDir
 git pull "https://github.com/MattSegal/RedditFollowerDeploy.git"
+cd $currentDir 
