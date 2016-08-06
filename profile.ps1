318772281e9ad3bbefc09b69992b005549dbf66c
MattSegal <mattdsegal@gmail.com>
2016-08-06 12:49:36 +1000

    Add Powershell profile and profile setup.

---
+++ profile.ps1
@@ -0,0 +1,7 @@
+function py     { python $args }
+function exp    { & "explorer" $args }
+$setup = "C:\SetupDeployment"
+$web = "C:\RedditFollower"
+$temp = "C:\Temp"
+
+cd $setup
---
+++ set_profile.ps1
@@ -0,0 +1,1 @@
+Copy-Item profile.ps1 $profile
