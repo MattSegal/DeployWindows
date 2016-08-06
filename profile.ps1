b9fca178f9fcac90915ad9c0a0da98f832379120
MattSegal <mattdsegal@gmail.com>
2016-08-06 12:51:09 +1000

    add choco profile to ps profile

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,9 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
 function py     { python $args }
 function exp    { & "explorer" $args }
 $setup = "C:\SetupDeployment"
