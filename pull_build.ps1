120d4932fae7f38f5648c57d510e5b148ddb25a9
MattSegal <mattdsegal@gmail.com>
2016-08-06 13:14:32 +1000

    I'm sure it'll work this time

--- pull_build.ps1
+++ pull_build.ps1
@@ -2,10 +2,16 @@ $currentDir = pwd
 
 # Make sure target dir exists
 $targetDir = "C:\RedditFollower"
-if (-Not (Test-Path $targetDir)) {
+if (-Not (Test-Path $targetDir)) 
+{
     New-Item $targetDir -type directory
-}
+    cd $targetDir
+    git clone https://github.com/MattSegal/RedditFollowerDeploy.git .
+} 
+else
+{
+    cd $targetDir
+    git pull "https://github.com/MattSegal/RedditFollowerDeploy.git"
+} 
 
-cd $targetDir
-git pull "https://github.com/MattSegal/RedditFollowerDeploy.git"
 cd $currentDir 
